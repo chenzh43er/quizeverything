2e380b4e897e66861a705093ea8f6a3eb9d52c61
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-07-07 23:02:08 +0800

    1

--- testname/js/google_adv.js
+++ testname/js/google_adv.js
@@ -1,173 +1,79 @@
 function returnADV_Block(){
-    // return `
-    // <ins class="adsbygoogle"
-    //                          style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
-    //                          data-ad-client="ca-pub-3481735481590354"
-    //                          data-ad-slot="7945649647"
-    //                          data-ad-format="auto"
-    //                          data-full-width-responsive="true"></ins>`
 }
 
 function returnADV_Block_test(){
-    // return `
-    // <ins class="adsbygoogle"
-    //                          style="display:block"
-    //                          data-ad-client="ca-pub-3481735481590354"
-    //                          data-ad-slot="7945649647"
-    //                          data-ad-format="auto"
-    //                          data-full-width-responsive="true"
-    //                          data-adtest="on"></ins>`
 }
 
 function returnZhongxiangGuding(){
     return `<p style="text-align: center">Advertisment</p>`
-//     let advHtml = `
-// <ins class="adsbygoogle"
-//      style="display:inline-block;width:300px;height:600px"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="3165596689"></ins>
-//             `;
-//     return advHtml;
 }
-//data-adtest="on" 复制该语句到为止为测试模式开启，删掉该语句则为正式广告
 
 function returnListAdv_1(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- list-1 -->
-// <ins class="adsbygoogle"
-//      style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="7760254681"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return `<div id='div-gpt-ad-1751529469858-1'  style="text-align: center"></div>`
 }
 
 function returnListAdv_2(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- list-2 -->
-// <ins class="adsbygoogle"
-//      style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="6447173015"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return `<div id='div-gpt-ad-1751540964165-0' style="text-align: center"></div>`
 }
 
 function returnListAdv_3(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- list-3 -->
-// <ins class="adsbygoogle"
-//      style="display:block"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="9263465087"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return `<div id='div-gpt-ad-1751540688195-0' style="text-align: center"></div>`
 }
 
 function returnListAdv_4(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- list-4 -->
-// <ins class="adsbygoogle"
-//      style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="6538913607"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return `<div id='div-gpt-ad-1751541451801-0' style="text-align: center"></div>`
 }
 
 function returnListAdv_5(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- list-5 -->
-// <ins class="adsbygoogle"
-//      style="display:block"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="3849231155"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return `<div id='div-gpt-ad-1751541584119-0' style="text-align: center"></div>`
 }
 
 function returnTestAdv_1(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- test-1 -->
-// <ins class="adsbygoogle"
-//      style="display:block"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="7543798111"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return ``
 }
 
 function returnTestAdv_2(){
     return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- test-2 -->
-// <ins class="adsbygoogle"
-//      style="display:inline-block;width:100%;height:250px"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="3912750264"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
 }
 
 function returnTestAdv_3(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- test-3 -->
-// <ins class="adsbygoogle"
-//      style="display:inline-block;width:100%;height:250px"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="5628915364"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return `<!-- /23284785848/mqqht/loadTestAdv_3 -->
+<div id='div-gpt-ad-1751661330868-0'>
+</div>`
 }
 
 function returnTestAdv_4(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- test-4 -->
-// <ins class="adsbygoogle"
-//      style="display:inline-block;width:300px;height:600px"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="2599668591"></ins>`
+    return `<!-- /23284785848/mqqht/loadTestAdv_4 -->
+<div id='div-gpt-ad-1751661226505-0'>
+</div>`
 }
 
 function returnResultAdv_1(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- result-1 -->
-// <ins class="adsbygoogle"
-//      style="display:block"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="1286586926"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return ``
 }
 
 function returnResultAdv_2(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- result-2 -->
-// <ins class="adsbygoogle"
-//      style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="5134091346"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return `<!-- /23284785848/result/loadResultAdv_2 -->
+<div id='div-gpt-ad-1751611776872-0' style="text-align: center">
+</div>`
 }
 
 function returnResultAdv_3(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- result-3 -->
-// <ins class="adsbygoogle"
-//      style="display:inline-block;width:100%;height:200px"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="2627347502"></ins>`
+    return `<!-- /23284785848/result/loadResultAdv_3 -->
+<div id='div-gpt-ad-1751614229847-0' style="text-align: center">
+</div>`
 }
 
 function returnResultAdv_4(){
-    return `<p style="text-align: center">Advertisment</p>`
-//     return `<!-- result-4 -->
-// <ins class="adsbygoogle"
-//      style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
-//      data-ad-client="ca-pub-3481735481590354"
-//      data-ad-slot="8973505259"
-//      data-ad-format="auto"
-//      data-full-width-responsive="true"></ins>`
+    return `<!-- /23284785848/result/loadResultAdv_4 -->
+<div id='div-gpt-ad-1751615792248-0' style="text-align: center">
+</div>`
+}
+
+function returnResultAdv_5(){
+    return `<!-- /23284785848/result/loadResultAdv_5 -->
+<div id='div-gpt-ad-1751626033131-0' style="text-align: center">
+</div>`
 }
 
 
